95584ffbb913e9e4655b6852f74313eb5457d726
amichailevy <amichailevy@Amichais-MacBook-Pro.local>
2022-10-29 09:48:06 -0400

    wip

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import notebooksList from '../components/NotebooksList.vue';
-import { authGuard } from '@auth0/auth0-vue';
-import BoardView from '../components/BoardView.vue';
-import NewBoard from '../components/NewBoard.vue';
 import HomeView from '../components/HomeView.vue';
 import CardDeck from '../components/CardDeck.vue';
 
 const routes = [
-  {
-    path: '/notebook/:notebookId',
-    name: 'Notebook',
-    component: BoardView,
-    props: true,
-  },
-  {
-    path: '/:notebookId/new',
-    name: 'New Notebook',
-    component: NewBoard,
-    props: true,
-  },
   {
     path: '/',
     name: 'Home',
